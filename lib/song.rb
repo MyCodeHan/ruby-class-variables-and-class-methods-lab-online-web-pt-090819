7686ae3aa2119a7dbea8d3ec78987a93b578dc91
Hannah Harrington <hannahharrington@outlook.com>
2019-11-11 17:59:47 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+   attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artist = []
+    @@genre = []
+
+def initialize (name,artist,genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count +- 1 
+  @@artist << artist
+  @@genre << genre
+end
+
+def self.count
+  @@count 
+ end 
+def self.artist
+    @@artist
+ end
+def self.genre
+    @@genre
+end 
+end 
