7994ef81bc979d2373e2ba53f5a0f7223c272500
Hannah Harrington <hannahharrington@outlook.com>
2019-11-11 18:22:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,37 +1,52 @@
 class Song
-   attr_accessor :name, :artist, :genre
-    @@count = 0
-    @@artist = []
-    @@genre = []
-  end 
+  attr_accessor :name, :artist, :genre
 
-def initialize (name,artist,genre)
-  @name = name
-  @artist = artist
-  @genre = genre
-  @@count +- 1 
-  @@artist << artist
-  @@genre << genre
-end
+  @@count = 0
+  @@genres = []
+  @@artists = []
 
-def self.count
-  @@count 
- end
-end 
-def self.artist
-    @@artist
- end
-end 
- def self.genre
-    @@genre
-end 
-def self.genre_count
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre] 
+      if genre_count[genre] #if the genre is already in the hash
         genre_count[genre] += 1
       else #if it's a new genre
         genre_count[genre] = 1
       end
     end
- 
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] #if the artist is already in the hash
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+end
